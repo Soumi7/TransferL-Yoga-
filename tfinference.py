b3dc7cbeec6b8b515de44a4b0f405eef849b5ba4
soumi7 <soumibardhan10@gmail.com>
2020-04-29 14:46:40 +0530

    inference file to check

---
+++ tfinference.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import h5py
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+IMG_SIZE=224
+model = load_model('/home/soumi/Downloads/best1.h5') 
+
+
+image_path="File2.jpg"
+img = image.load_img(image_path, target_size=(IMG_SIZE, IMG_SIZE))
+img = image.img_to_array(img)
+img = np.expand_dims(img, axis=0)
+
+
+result=model.predict_classes(img)
+print(result)
+predicted_class = ("bridge", "child","tristep1","tristep2","tristep3")[result[0]]
+print(predicted_class)
+
+
+
+
+
+
+
+'''
+
+plt.title(get_label_name(result[0][0]))
+plt.show()
+'''
